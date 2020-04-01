19a12f0085bf12c60fbf282d6201e67e8222fa71
thiago william <thiago.will.silva@gmail.com>
2020-04-01 14:07:18 +0100

    Updates

---
+++ precache-manifest.459cd8d38f5700686c1e5c0678c3c809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5408a66ca0844474c997a8e2609531cd",
+    "url": "/api-github/index.html"
+  },
+  {
+    "revision": "ef1da1ed285a86abaeea",
+    "url": "/api-github/static/js/2.848e1b7b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/api-github/static/js/2.848e1b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762f179e9de5aa005fc7",
+    "url": "/api-github/static/js/main.7b4fc2dc.chunk.js"
+  },
+  {
+    "revision": "623ef8722d7c27b619c5",
+    "url": "/api-github/static/js/runtime-main.6cb44cd8.js"
+  }
+]);
